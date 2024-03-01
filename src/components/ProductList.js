cd647a8f5cfa245559000790685659f5e0cedd92
yebente <yebente@gmail.com>
2024-03-01 14:14:52 +0300

    Updated ProductList js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,8 +4,14 @@ import Product from './Product';
 function ProductList({ products }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
+      {products.map(product => (
+        <Product
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          image={product.image}
+        />
       ))}
     </div>
   );
