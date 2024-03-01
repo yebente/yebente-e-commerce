5d8ef4ce2b3cdd3c1e4ee1533e563aeaf9a8253f
yebente <yebente@gmail.com>
2024-03-01 12:54:29 +0300

    Home js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-2xl font-bold mb-4">Welcome to Yebente</h2>
+      <p className="text-lg">Find unique products from sellers all around the world.</p>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+}
+
+export default Home;
