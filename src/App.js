e083abb1118d5159785dfc7cf2bac3e1180623f9
yebente <yebente@gmail.com>
2024-03-01 14:01:23 +0300

    App js Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
 import SellPage from './pages/SellPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
@@ -8,25 +10,26 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Router>
-      <div className="font-sans bg-gray-100">
+      <div className="flex flex-col min-h-screen">
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/sell" component={SellPage} />
-          <Route path="/product/:id" component={ProductDetailsPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={SignupPage} />
-          <Route path="/cart" component={CartPage} />
-          <Route path="/checkout" component={CheckoutPage} />
-          {/* Add more routes for additional pages */}
-        </Switch>
+        <main className="flex-grow">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/sell" component={SellPage} />
+            <Route path="/product/:id" component={ProductDetailsPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/signup" component={SignupPage} />
+            <Route path="/cart" component={CartPage} />
+            <Route path="/checkout" component={CheckoutPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
         <Footer />
       </div>
     </Router>
